13122b242932074d90989ba65cb180827790122b
Danniely Santos <dannielyingrid@gmail.com>
2020-08-18 15:31:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,21 @@
 class Book
   def initialize(title)
+    @tittle = tittle 
   end
+  
   def tittle 
+    @tittle
   end
+  
+  def author=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+    
+  
 end 
 
 
